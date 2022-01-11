9b35f0ffcbd504fa805346ea27c401a1b7282325
David Batista <davidbatista2@outlook.com>
2022-01-11 13:36:13 -0300

    Meu primeiro grande site criado com HTML e CSS, para fixar meus conhecimentos

---
+++ js/owl/setup.js
@@ -0,0 +1,38 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:70,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        800:{
+            items:3
+        },
+        1000:{
+            items:4
+        },
+        1100:{
+            items:4
+        },
+        1250:{
+            items:4
+        },
+        1400:{
+            items:4
+        },
+        1600:{
+            items:4
+        },
+        1700:{
+            items:5
+        },
+        1800:{
+            items:7
+        },
+        
+    }
+})
